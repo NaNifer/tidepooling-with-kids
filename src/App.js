85c4afff34945ef69d91752ddd8d49fccae75095
Nifer Kilakila <nifercritter@gmail.com>
2023-02-28 15:37:43 -0800

    top buttons start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import TopButtons from './components/TopButtons';
 
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-indigo-400 to-cyan-700">
- 
+      <TopButtons />
     </div>
   );
 }
