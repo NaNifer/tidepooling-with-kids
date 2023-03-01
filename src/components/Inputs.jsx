d8f64e3ff06f6aa947a55bbd85c04c0ae12b30d1
Nifer Kilakila <nifercritter@gmail.com>
2023-02-28 20:56:27 -0800

    input tidalwave

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
-
-
+import React from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
 function Inputs() {
   return (
-    <div className='flex flex-row justify-center my-6'>Inputs</div>
-  )
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          className="text-xl capitalize font-light p-2 w-full shadow-xl focus:outline-none placeholder:lowercase"
+          placeholder="search for city..."
+        />
+        <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
+        <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
+      </div>
+    </div>
+  );
 }
 
-export default Inputs
+export default Inputs;
