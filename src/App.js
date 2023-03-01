3f5604dd2a7291b9bd3ae731414411d1e0b0d240
Nifer Kilakila <nifercritter@gmail.com>
2023-03-01 12:54:41 -0800

    Testing out hamburger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import CityButtons from './components/CityButtons';
 import CitySearch from './components/CitySearch';
+import Header from './components/Header'
 
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-indigo-400 to-cyan-700">
+    <div className="mx-auto max-w-screen-sm mt-4 py-5 px-32 bg-gradient-to-br from-indigo-400 to-cyan-700">
+      <Header />
       <CityButtons />
       <CitySearch />
     </div>
