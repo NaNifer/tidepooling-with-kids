e15859fa95e82e5029af5f2305f9587ecddfa521
Nifer Kilakila <nifercritter@gmail.com>
2023-03-02 09:31:44 -0800

    testing out timepickers

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
+import TimePicker from "react-dropdown-timepicker";
 
 function CitySearch() {
   return (
@@ -20,12 +21,16 @@ function CitySearch() {
         />
       </div>
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-      <input
+        <input
           type="text"
           className="text-xl capitalize font-light p-2 w-full shadow-xl focus:outline-none placeholder:lowercase"
           placeholder="enter city for data..."
         />
-      </div>
+        {/* <TimePicker
+          time={this.state.time}
+          onChange={this.handleTimeChange.bind(this)}
+        />
+      </div> */}
     </div>
   );
 }
