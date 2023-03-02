1d0f7b6860f83293d35ef67e6671d08ea22f67a2
Nifer Kilakila <nifercritter@gmail.com>
2023-03-01 22:00:53 -0800

    added modal to nav search dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,69 @@
-// import { useState } from "react"; 
+import { useState } from "react";
 // import Anemone from '../assets/sea-anemone-test.png'
-import Cartoon from '../assets/cartoon-anemone.png'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import Cartoon from "../assets/cartoon-anemone.png";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Form,
+  Modal,
+} from "react-bootstrap";
 
 export default function Header() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
           <img src={Cartoon} alt="logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Current Conditions" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Fort Bragg</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Current Conditions" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Fort Bragg</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
                 Point Reyes
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Half Moon Bay</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  New Search
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Pacifica</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={handleShow}>
+                New Search
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Modal size="sm" show={show} onHide={handleClose}>
+            <Modal.Body>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Enter city for current conditions</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="e.g., Half Moon Bay"
+                    autoFocus
+                  />
+                </Form.Group>
+              </Form>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Enter
+              </Button>
+            </Modal.Body>
+          </Modal>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
