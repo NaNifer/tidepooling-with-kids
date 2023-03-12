528498a499a307940b9e48503a0f62254fe7137b
Nifer Kilakila <nifercritter@gmail.com>
2023-03-11 20:18:56 -0800

    Sketching out weather api feature

---
+++ src/utils/weatherAPI.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const weatherAPI = (query) =>
+  axios.get(``);
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default weatherAPI;
