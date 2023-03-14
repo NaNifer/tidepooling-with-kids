f85de6e58b722f79b3ee93216adc8beb7c5c062c
Nifer Kilakila <nifercritter@gmail.com>
2023-03-13 22:08:58 -0700

    Sketching out tide API, added dotenv

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -1,7 +1,26 @@
 import axios from 'axios';
+require('dotenv').config();
+
+const options = {
+  method: 'GET',
+  url: 'https://noaa-tides.p.rapidapi.com/stations/9411340/tides',
+  params: {date: '20220625'},
+  headers: {
+    'X-RapidAPI-Key': process.env.TIDE_APIKEY,
+    'X-RapidAPI-Host': 'noaa-tides.p.rapidapi.com'
+  }
+};
 
 const weatherAPI = (query) =>
-  axios.get(``);
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default weatherAPI;
+
+
+
+
